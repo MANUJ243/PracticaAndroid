920355a3a03f9479a7d792d53a5cec2c2f0654b2
Manuel <manuj243@gmail.com>
2018-02-09 21:05:22 +0100

    Arreglo del Login de Twitter. En Firebase no aparece nombre???

--- app/src/main/java/com/example/manue/practicaandroid/LoginActivity.java
+++ app/src/main/java/com/example/manue/practicaandroid/LoginActivity.java
@@ -1,20 +1,16 @@
 package com.example.manue.practicaandroid;
 
 import android.content.Intent;
-import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import com.firebase.ui.auth.AuthUI;
 import com.firebase.ui.auth.IdpResponse;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-
 import java.util.Arrays;
 import java.util.List;
 
-public class LoginActivity extends AppCompatActivity /*implements GoogleApiClient.OnConnectionFailedListener */{
+public class LoginActivity extends AppCompatActivity {
     private static final int RC_SIGN_IN = 123;
 
     @Override
