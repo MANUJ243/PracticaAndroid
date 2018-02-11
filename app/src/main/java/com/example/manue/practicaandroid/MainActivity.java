3913bace6a5c3436cefcc519959ac5c1bbc3d0c2
Manuel <manuj243@gmail.com>
2018-02-11 17:26:57 +0100

    Anadido Modulo de publicidad: Banner.

--- app/src/main/java/com/example/manue/practicaandroid/MainActivity.java
+++ app/src/main/java/com/example/manue/practicaandroid/MainActivity.java
@@ -7,14 +7,24 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ProgressBar;
 
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
+
 public class MainActivity extends AppCompatActivity {
     Button boton1;
     ProgressBar progressBar;
+    private AdView mAdView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        MobileAds.initialize(this, "ca-app-pub-8810149351311606~4636982690");
+
+        mAdView = findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        mAdView.loadAd(adRequest);
         //boton1 = findViewById(R.id.entrenamientoButton);
         //progressBar = findViewById(R.id.loadingBar);
     }
